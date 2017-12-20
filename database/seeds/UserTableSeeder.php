1f3f712e3ca55a3a64553dafc8b26ee30020142a
mr-exception <drabandi1996@gmail.com>
2017-12-20 15:56:29 +0330

    user tables seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SlideTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      /*
+        fake data
+      */
+      $data = [
+        [
+          "id"                => 1,
+          "username"          => "admin",
+          "email"             => "admin@gmail.com",
+          "password"          => "$2y$10$8dNxMMXyrTj9qSPrnMdk/eKXpAUpeaI.B1pxQkhSooh8/V9O1NQHW",
+          "status"            => 1,
+          "permission_level"  => 2,
+        ],
+      ];
+      foreach($data as $row)
+        DB::table('users')->insert($row);
+    }
+}
