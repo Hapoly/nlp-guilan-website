7e97250d411a9b6f3e60b9e8e0ac96289066a8e4
mr-exception <drabandi1996@gmail.com>
2017-12-24 22:50:01 +0330

    slides index page redisgn

--- resources/views/admin/slides/index.blade.php
+++ resources/views/admin/slides/index.blade.php
@@ -3,13 +3,31 @@
 slides
 @endsection
 @section('content')
-<a href="{{route('slides.create')}}">create</a>
-<form action="{{route('slides.index',['sort' => $sort ,'slide' => 1])}}" method="get">
-  <input name="search" placeholder="search..." value="{{$search != '###'? $search: ''}}" />
-  <button type="submit">search</button>
-</form>
-<table>
-  <thead>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
+    <div class="border-radius">
+    <form action="{{route('slides.index',['sort' => $sort ,'slide' => 1])}}" method="get">
+      <div class="row">
+        <div class="col-lg-8" style="margin-top:13px;">
+          <div class="input-group">
+          <input  class="form-control"  name="search" placeholder="search..." value="{{$search != '###'? $search: ''}}" />
+            <span class="input-group-btn">
+              <button class="btn btn-secondary" type="submit">Go!</button>
+            </span>
+          </div>
+       </div>
+       <div class="col-lg-4">
+          <button type="button" class="btn btn-outline-secondary">
+          <a href="{{route('slides.create')}}">create</a>
+          </button>
+      </div>
+      </div>
+     </form>
+    
+
+<table class="table">
+  <thead class="grey">
     <th><a href="{{route('slides.index',['search' => $search,'sort' => 'id'    ,'page' => $slides->currentPage()])}}">id</a></th>
     <th><a href="{{route('slides.index',['search' => $search,'sort' => 'title' ,'page' => $slides->currentPage()])}}">title</a></th>
     <th><a href="{{route('slides.index',['search' => $search,'sort' => 'status','page' => $slides->currentPage()])}}">status</a></th>
@@ -22,18 +40,24 @@ slides
         <td>{{$slide->title}}</td>
         <td>{{$slide->status}}</td>
         <td>
-          <a href="{{route('slides.show', ['slide' => $slide])}}">show</a>
-          <a href="{{route('slides.edit', ['slide' => $slide])}}">edit</a>
-          <form action="{{route('slides.destroy', ['slide' => $slide])}}" method="POST">
+          <a href="{{route('slides.show', ['slide' => $slide])}}">
+          <i class="fa fa-file-text-o"></i></a>
+          <a href="{{route('slides.edit', ['slide' => $slide])}}">
+          <i class="fa fa-edit"></i></a>
+          <form action="{{route('slides.destroy', ['slide' => $slide])}}" method="POST" class="trash-icon">
             {{ method_field('DELETE') }}
             {{ csrf_field() }}
-            <button type="submit">remove</button>
+            <i class="fa fa-trash-o"></i>
           </form>
         </td>
       </tr>
     @endforeach
-  </tbody>
-</table>
+    </tbody>
+    </table>
+    </div>
+  </div>
+</div>
+</div>
 {{ $slides->links() }}
 
 @endsection
