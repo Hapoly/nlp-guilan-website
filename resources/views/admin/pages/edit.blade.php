a877fcb064e2668d509f7f5199a59a69364ac6ea
sadafN <n.shell120@gmail.com>
2018-01-02 14:27:52 +0330

    edit ro bebin

--- resources/views/admin/pages/edit.blade.php
+++ resources/views/admin/pages/edit.blade.php
@@ -3,24 +3,55 @@
 edit page $page->title
 @endsection
 @section('content')
-<form action="{{route('pages.update', ['page' => $page])}}" method="POST">
-  {{csrf_field()}}
-  {{ method_field('PUT') }}
-  @if($errors->has('title'))
-    {{$errors->first('title')}}
-  @endif
-  <input name="title" placeholder="page title" value="{{$errors->has('title')? old('title'): $page->title}}"/><br>
-  @if($errors->has('body'))
-    {{$errors->first('body')}}
-  @endif
-  <textarea name="body" line="3" placeholder="page body"/>{{$errors->has('body')? old('body'): $page->body}}</textarea><br>
-  @if($errors->has('status'))
-    {{$errors->first('status')}}
-  @endif
-  <select name="status">
-    <option value="1" {{$errors->has('status')? (old('status') == 1? 'selected' : ''):($page->status == 1? 'selected': '')}}>published</option>
-    <option value="2" {{$errors->has('status')? (old('status') == 2? 'selected' : ''):($page->status == 2? 'selected': '')}}>unpublished</option>
-  </select><br>
-  <button type="submit">save</button>
-</form>
+<div class="container">
+  <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
+    <div class="card">
+      <div class="edit-form">
+        <form action="{{route('pages.update', ['page' => $page])}}" method="POST">
+          {{csrf_field()}}
+          {{ method_field('PUT') }}
+          @if($errors->has('title'))
+          {{$errors->first('title')}}
+          @endif
+          <div class="form-group row">
+            <div class="col-3 ">
+              <label for="title" class="col-form-label">page title:    </label>
+            </div>
+            <div class="col-8">
+              <input name="title" class="form-control" id="title" value="{{$errors->has('title')? old('title'): $page->title}}"/>
+            </div>
+          </div>
+          @if($errors->has('body'))
+          {{$errors->first('body')}}
+          @endif
+          <div class="form-group row">
+            <div class="col-2">
+              <label for="title" class="col-form-label"> body: </label>
+            </div>
+            <div class="col-9">
+              <textarea name="body" class="form-control" line="3"/>{{$errors->has('body')? old('body'): $page->body}}</textarea><br>
+            </div>
+          </div>
+          @if($errors->has('status'))
+          {{$errors->first('status')}}
+          @endif
+          <div class="form-group row">
+            <div class="col-2">
+              <label for="status">
+                status:
+              </label>
+            </div>
+            <div class="col-9">
+              <select name="status" class="form-control">
+                <option value="1" {{$errors->has('status')? (old('status') == 1? 'selected' : ''):($page->status == 1? 'selected': '')}}>published</option>
+                <option value="2" {{$errors->has('status')? (old('status') == 2? 'selected' : ''):($page->status == 2? 'selected': '')}}>unpublished</option>
+              </select><br>
+            </div>
+          </div>
+          <button type="submit" class="save">save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/admin/pages/show.blade.php
+++ resources/views/admin/pages/show.blade.php
@@ -3,23 +3,31 @@
 {{$page->title}}
 @endsection
 @section('content')
-<div>
-  <b>title:</b>{{$page->title}}
+<div class="container">
+  <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
+  <div class="card">
+  <div class="card-block">
+  <div>
+    <b>title:</b>{{$page->title}}
+  </div>
+  <div>
+    <b>body:</b>{{$page->body}}
+  </div>
+  <div>
+    <b>status:</b>{{$page->get_status()}}
+  </div>
+  <div>
+    <a href="{{route('pages.edit', ['page' => $page])}}">edit this page</a>
+  </div>
+  <div>
+    <form action="{{route('pages.destroy', ['page' => $page])}}" method="POST">
+      {{ method_field('DELETE') }}
+      {{ csrf_field() }}
+      <button type="submit">remove this page</button>
+    </form>
+  </div>
 </div>
-<div>
-  <b>body:</b>{{$page->body}}
 </div>
-<div>
-  <b>status:</b>{{$page->get_status()}}
 </div>
-<div>
-  <a href="{{route('pages.edit', ['page' => $page])}}">edit this page</a>
-</div>
-<div>
-  <form action="{{route('pages.destroy', ['page' => $page])}}" method="POST">
-    {{ method_field('DELETE') }}
-    {{ csrf_field() }}
-    <button type="submit">remove this page</button>
-  </form>
 </div>
 @endsection
