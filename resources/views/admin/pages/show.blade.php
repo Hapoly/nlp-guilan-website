7407bf2a87b8927e6200d8c40b3dca40f66eabb8
sadafN <n.shell120@gmail.com>
2018-01-07 21:44:01 +0330

    lkjkjhju

--- resources/views/admin/pages/show.blade.php
+++ resources/views/admin/pages/show.blade.php
@@ -7,33 +7,33 @@
   <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
     <table class="show-table" style="width:100%">
       <tr>
-          <th>title </th>
-          <td>{{$page->title}}</td>
+        <th>title </th>
+        <td>{{$page->title}}</td>
       </tr>
       <tr>
-          <th>body </th>
-          <td>{{$page->body}}</td>
+        <th>body </th>
+        <td>{{$page->body}}</td>
       </tr>
       <tr>
-          <th>status  </th>
-          <td>{{$page->get_status()}}</td>
+        <th>status  </th>
+        <td>{{$page->get_status()}}</td>
       </tr>
       <tr>
-          <th>operation </th>
-          <td>
-              <div class="container" style="margin-left:110px;">
-                  <div class="row" style=" margin-top: 0">
-                    <a href="{{route('pages.edit', ['page' => $page])}}">
-                      <i class="fa fa-edit show-edit"></i>
-                    </a>
-                    <form action="{{route('pages.destroy', ['page' => $page])}}" method="POST">
-                      {{ method_field('DELETE') }}
-                      {{ csrf_field() }}
-                      <i class="fa fa-trash-o"></i>
-                    </form>
-                </div>
+        <th>operation </th>
+        <td>
+            <div class="container" style="margin-left:110px;">
+                <div class="row" style=" margin-top: 0">
+                  <a href="{{route('pages.edit', ['page' => $page])}}">
+                    <i class="fa fa-edit show-edit"></i>
+                  </a>
+                  <form action="{{route('pages.destroy', ['page' => $page])}}" method="POST">
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <i class="fa fa-trash-o"></i>
+                  </form>
               </div>
-          </td>
+            </div>
+        </td>
       </tr>
     </table>
   </div>
--- resources/views/admin/slides/show.blade.php
+++ resources/views/admin/slides/show.blade.php
@@ -5,34 +5,41 @@
 @section('content')
 <div class="container">
   <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
-    <div class="card">
-      <div class="card-block">
-        <div>
-         <b>title:</b>{{$slide->title}}
-         </div>
-        <div>
-          <b>caption:</b>{{$slide->caption}}
-        </div>
-        <div>
-          <b>status:</b>{{$slide->get_status()}}
-        </div>
-        <div>
-          <img src="{{asset('storage/slides/' . $slide->image_url)}}" />
-        </div>
-        <div class="container">
-          <div class="row">
-            <a href="{{route('slides.edit', ['slide' => $slide])}}">
-              <i class="fa fa-edit show-edit"></i>
-            </a>
-            <form action="{{route('slides.destroy', ['slide' => $slide])}}" method="POST">
-              {{ method_field('DELETE') }}
-              {{ csrf_field() }}
-              <i class="fa fa-trash-o"></i>
-            </form>
-          </div>
-        </div>
+    <table class="show-table" style="width:100%">
+      <div class="card-show">
+        <img src="{{asset('storage/slides/' . $slide->image_url)}}" />
       </div>
-    </div>
+      <tr>
+        <th>title </th>
+        <td>{{$slide->title}}</td>
+      </tr>
+      <tr>
+        <th>caption </th>
+        <td>{{$slide->caption}}</td>
+      </tr>
+      <tr>
+        <th>status  </th>
+        <td>{{$slide->get_status()}}</td>
+      </tr>
+      <tr>
+        <th>operation </th>
+        <td>
+            <div class="container" style="margin-left:110px;">
+                <div class="row" style=" margin-top: 0">
+                  <a href="{{route('slides.edit', ['slide' => $slide])}}">
+                    <i class="fa fa-edit show-edit"></i>
+                  </a>
+                  <form action="{{route('slides.destroy', ['slide' => $slide])}}" method="POST">
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <i class="fa fa-trash-o"></i>
+                  </form>
+              </div>
+            </div>
+        </td>
+      </tr>
+    </table>
+     
   </div>
 </div>
 @endsection
