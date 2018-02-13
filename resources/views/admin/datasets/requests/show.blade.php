dacfe2abc96dc154a8418abaaa38de310ed6f6f4
mr-exception <drabandi1996@gmail.com>
2018-02-13 11:42:27 +0330

    some changes in show

--- resources/views/admin/datasets/requests/show.blade.php
+++ resources/views/admin/datasets/requests/show.blade.php
@@ -4,33 +4,34 @@
 @endsection
 @section('content')
 <div class="container">
-  <div class="col-lg-6 col-md-6 col-sm-8 offset-lg-3 offset-md-3 offset-sm-2">
-    <table class="show-table" style="width:100%">
-      <tr>
-        <th>requestor name </th>
-        <td>{{$dataset_request->name}}</td>
-      </tr>
-      <tr>
-        <th>requestor email </th>
-        <td>{{$dataset_request->email}}</td>
-      </tr>
-      <tr>
-        <th>use case </th>
-        <td>{{$dataset_request->use_case}}</td>
-      </tr>
-      <tr>
-        <th>email </th>
-        <td>{{$dataset_request->email}}</td>
-      </tr>
-      <tr>
-        <th>dataset </th>
-        <td><a href="{{route('datasets.show', ['dataset' => $dataset_request->datset_set])}}">{{$dataset_request->dataset->publication->title}}</a></td>
-      </tr>
-      <tr>
-        <th>publication </th>
-        <td><a href="{{route('publications.show', ['publication' => $dataset_request->dataset->publication])}}">{{$dataset_request->dataset->publication->title}}</a></td>
-      </tr>
-    </table>
+  <div class="row">
+    <div class="col-lg-8 col-md-8 col-sm-8 offset-lg-2 offset-md-2 offset-sm-2">
+      <table class="show-table" style="width:100%">
+        <tr>
+          <th>requestor name </th>
+          <td>{{$dataset_request->name}}</td>
+        </tr>
+        <tr>
+          <th>requestor email </th>
+          <td>{{$dataset_request->email}}</td>
+        </tr>
+        <tr>
+          <th>email </th>
+          <td>{{$dataset_request->email}}</td>
+        </tr>
+        <tr>
+          <th>dataset </th>
+          <td><a href="{{route('datasets.show', ['dataset' => $dataset_request->datset_set])}}">{{$dataset_request->dataset->publication->title}}</a></td>
+        </tr>
+        <tr>
+          <th>publication </th>
+          <td><a href="{{route('publications.show', ['publication' => $dataset_request->dataset->publication])}}">{{$dataset_request->dataset->publication->title}}</a></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div class="row">
+    <p>{{$dataset_request->use_case}}</p>
   </div>
 </div>
 
